8973cf79f0ab072985f8857d9a3c6adc735610a1
Roma <romachne@gmail.com>
2023-09-17 02:31:42 +0300

    Add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-weight: lighter;
+  color: bisque;
+  font-size: 12rem;
+  margin: 0;
+`
+
+const Header = () => <header><Title>todos</Title></header>
+
+export default Header
