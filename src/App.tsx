336ce12a1b9687288ece60cd3c72f3580aea74be
Roma <romachne@gmail.com>
2023-12-06 12:48:57 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ function App() {
                       return true;
                   }
                 })
-                .map((item, index) => (
+                .map((item) => (
                   <TodoItem
                     key={item.id}
                     item={item}
